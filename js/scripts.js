9b40931b6d22a26d65a3919a2cdadc84aef14f3b
cschreiner0 <75751364+cschreiner0@users.noreply.github.com>
2021-01-27 14:03:33 -0800

    Setup Randomization * Set up random background colors * Refactored to use array.length

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
 console.log("OK");
+
+//store background classes
+const backgrounds = ["bg0", "bg1", "bg2"];
+
+//Get random numbers
+var numRand;
+numRand = Math.floor(Math.random() * backgrounds.length);
+
+//Get element
+let pageBackgound = document.querySelector("body");
+
+//Set class name from stored values
+pageBackgound.className = backgrounds[numRand];
